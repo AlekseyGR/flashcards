f5b2c74f1243b291e31f39281fc8eda2480f7cd5
AlekGR <alek.grischenko@gmail.com>
2015-11-22 14:59:02 +0300

    Refactoring handling correct answer

--- app/controllers/dashboard/trainer_controller.rb
+++ app/controllers/dashboard/trainer_controller.rb
@@ -19,14 +19,7 @@ class Dashboard::TrainerController < Dashboard::BaseController
     check_result = @card.check_translation(trainer_params[:user_translation])
 
     if check_result[:state]
-      if check_result[:distance] == 0
-        flash[:notice] = t(:correct_translation_notice)
-      else
-        flash[:alert] = t 'translation_from_misprint_alert',
-                          user_translation: trainer_params[:user_translation],
-                          original_text: @card.original_text,
-                          translated_text: @card.translated_text
-      end
+      handle_correct_answer(check_result[:distance])
       redirect_to trainer_path
     else
       flash[:alert] = t(:incorrect_translation_alert)
@@ -41,6 +34,17 @@ class Dashboard::TrainerController < Dashboard::BaseController
       current_user.current_block.cards.first_repeating_or_pending_card
     else
       current_user.cards.first_repeating_or_pending_card
+    end
+  end
+
+  def handle_correct_answer(distance)
+    if distance == 0
+      flash[:notice] = t(:correct_translation_notice)
+    else
+      flash[:alert] = t('translation_from_misprint_alert',
+                        user_translation: trainer_params[:user_translation],
+                        original_text: @card.original_text,
+                        translated_text: @card.translated_text)
     end
   end
 
