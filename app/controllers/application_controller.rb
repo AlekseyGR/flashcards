33f98ed25bc78823b2f56b11bb45ea98bee243ee
AlekGR <alek.grischenko@gmail.com>
2015-12-06 12:55:09 +0300

    Configure active admin for authorization actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,10 @@ class ApplicationController < ActionController::Base
     redirect_to login_path unless current_user
   end
 
+  def access_denied(exception)
+    redirect_to root_path, alert: exception.message
+  end
+
   private
 
   def set_locale
