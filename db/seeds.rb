773a60988f14fcc3d842987002b71c0d02f690dc
AlekGR <alek.grischenko@gmail.com>
2015-12-07 21:05:10 +0300

    Fix seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,14 +11,6 @@
 require 'nokogiri'
 require 'open-uri'
 
-doc = Nokogiri::HTML(open('http://www.learnathome.ru/blog/100-beautiful-words'))
-
-doc.search('//table/tbody/tr').each do |row|
-  original = row.search('td[2]/p')[0].content.downcase
-  translated = row.search('td[1]/p')[0].content.downcase
-  Card.create(original_text: original, translated_text: translated, user_id: 17)
-end
-
 default_user = User.create(
   email: 'admin@flashcards.com',
   password: 'password',
@@ -26,3 +18,11 @@ default_user = User.create(
 )
 
 default_user.add_role(:admin)
+
+doc = Nokogiri::HTML(open('http://www.learnathome.ru/blog/100-beautiful-words'))
+
+doc.search('//table/tbody/tr').each do |row|
+  original = row.search('td[2]/p')[0].content.downcase
+  translated = row.search('td[1]/p')[0].content.downcase
+  Card.create(original_text: original, translated_text: translated, user_id: default_user.id)
+end
