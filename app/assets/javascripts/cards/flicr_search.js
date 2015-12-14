a78725821badae43dc68199ffd45a3400f76e98c
AlekGR <alek.grischenko@gmail.com>
2015-12-14 07:27:22 +0300

    Ajax handling search request.

--- app/assets/javascripts/cards/flicr_search.js
+++ app/assets/javascripts/cards/flicr_search.js
@@ -2,4 +2,15 @@ $(document).ready(function(){
   $('.flickr-button').click(function(){
     $('#flickr-search-form').toggleClass('hidden');
   });
+  $('#flickr-search-run').click(function(){
+    var search_text = $('input[name="search"]').val()
+    if (search_text.length > 0){
+      $.ajax({
+        url: '/image_search',
+        data: { search: search_text }
+      })
+    } else {
+      // show error message
+    }
+  });
 });
