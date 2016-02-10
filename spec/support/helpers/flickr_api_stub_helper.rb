50c2a20442b2ac37a9ffe0b2eb91d5a045430e94
AlekGR <alek.grischenko@gmail.com>
2016-02-10 11:30:13 +0300

    Add stub data for flickr responce.

---
+++ spec/support/helpers/flickr_api_stub_helper.rb
@@ -0,0 +1,65 @@
+module FlickrApiStubHelper
+  def flickr_responce_stub
+    recent_responce =
+      { "photos":
+        { "page": 1,
+          "pages": "100",
+          "perpage": 10,
+          "total": "1000",
+          "photo": [
+            { "id": "24302016344", "owner": "134575593@N05", "secret": "72f2a42ea1", "server": "1631", "farm": 2, "title": "London's pretty houses. #london #photooftheday #londontown #travel #prettylittlelondon", "ispublic": 1, "isfriend": 0, "isfamily": 0 },
+            { "id": "24302016734", "owner": "137981303@N02", "secret": "a8913eb2a5", "server": "1632", "farm": 2, "title": "⚪️", "ispublic": 1, "isfriend": 0, "isfamily": 0 },
+            { "id": "24305771763", "owner": "94358006@N03", "secret": "1daffca263", "server": "1496", "farm": 2, "title": "", "ispublic": 1, "isfriend": 0, "isfamily": 0 },
+            { "id": "24305772243", "owner": "95829767@N05", "secret": "67b295e355", "server": "1496", "farm": 2, "title": "2016-02-10_03-59-25", "ispublic": 1, "isfriend": 0, "isfamily": 0 },
+            { "id": "24637134960", "owner": "135924320@N03", "secret": "6ec863eaee", "server": "1583", "farm": 2, "title": "Venus Swivel Bar Stool Bear Suede", "ispublic": 1, "isfriend": 0, "isfamily": 0 },
+            { "id": "24637135130", "owner": "10581108@N00", "secret": "f4376fbd20", "server": "1554", "farm": 2, "title": "b baseball", "ispublic": 1, "isfriend": 0, "isfamily": 0 },
+            { "id": "24814660252", "owner": "130778244@N07", "secret": "8ca0305101", "server": "1716", "farm": 2, "title": "للبيع خصي   العمر 7 سنوات   مخلص 80 و 120   مطلوب 10 آلاف   المعاينه العين سيح حرز  للتواصل 00971506969998 بو محمد", "ispublic": 1, "isfriend": 0, "isfamily": 0 },
+            { "id": "24839282541", "owner": "7983835@N05", "secret": "417a30282a", "server": "1507", "farm": 2, "title": "Welcome to The Aria Resort and Casino", "ispublic": 1, "isfriend": 0, "isfamily": 0 },
+            { "id": "24906312206", "owner": "98815300@N05", "secret": "ebf473f7c3", "server": "1514", "farm": 2, "title": "January ❤️", "ispublic": 1, "isfriend": 0, "isfamily": 0 },
+            { "id": "24932619875", "owner": "94775320@N04", "secret": "0f32664be6", "server": "1441", "farm": 2, "title": "すべての写真-139", "ispublic": 1, "isfriend": 0, "isfamily": 0 }
+          ] }, "stat": "ok" }
+
+    search_response =
+      { "photos": { "page": 1, "pages": "59298", "perpage": 10, "total": "592980",
+        "photo": [
+          { "id": "24932258435", "owner": "96711656@N07", "secret": "435b9810c8", "server": "1458", "farm": 2, "title": "CBS201602100830.jpg", "ispublic": 1, "isfriend": 0, "isfamily": 0 },
+          { "id": "24905333176", "owner": "81996688@N07", "secret": "03eb9f6905", "server": "1490", "farm": 2, "title": "Formula One Testing", "ispublic": 1, "isfriend": 0, "isfamily": 0 },
+          { "id": "24564007179", "owner": "81996688@N07", "secret": "76504d5923", "server": "1500", "farm": 2, "title": "Formula One Testing", "ispublic": 1, "isfriend": 0, "isfamily": 0 },
+          { "id": "24813674542", "owner": "81996688@N07", "secret": "89291c8fc1", "server": "1592", "farm": 2, "title": "Formula One Testing", "ispublic": 1, "isfriend": 0, "isfamily": 0 },
+          { "id": "24813551652", "owner": "96711656@N07", "secret": "142519128e", "server": "1538", "farm": 2, "title": "CBS201602100730.jpg", "ispublic": 1, "isfriend": 0, "isfamily": 0 },
+          { "id": "24838108311", "owner": "19907396@N00", "secret": "099272bbb5", "server": "1493", "farm": 2, "title": "Mireia", "ispublic": 1, "isfriend": 0, "isfamily": 0 },
+          { "id": "24300750114", "owner": "131891481@N05", "secret": "7e884b9986", "server": "1619", "farm": 2, "title": "DSC_0093", "ispublic": 1, "isfriend": 0, "isfamily": 0 },
+          { "id": "24304500903", "owner": "131891481@N05", "secret": "11b9ef705d", "server": "1638", "farm": 2, "title": "DSC_0193", "ispublic": 1, "isfriend": 0, "isfamily": 0 },
+          { "id": "24838013681", "owner": "131891481@N05", "secret": "efa5682a8a", "server": "1640", "farm": 2, "title": "DSC_0182", "ispublic": 1, "isfriend": 0, "isfamily": 0 },
+          { "id": "24813391572", "owner": "131891481@N05", "secret": "288f2d6441", "server": "1533", "farm": 2, "title": "DSC_0147", "ispublic": 1, "isfriend": 0, "isfamily": 0 }
+        ] }, "stat": "ok" }
+
+    @searched_photo_result = [
+      "https://farm2.staticflickr.com/1667/24503450029_3a043b85b1_q.jpg",
+      "https://farm2.staticflickr.com/1557/24753089122_7103b52410_q.jpg",
+      "https://farm2.staticflickr.com/1485/24574860910_498f9b0e25_q.jpg",
+      "https://farm2.staticflickr.com/1622/24844673716_1d598fea4b_q.jpg",
+      "https://farm2.staticflickr.com/1566/24777399031_5e69c152c0_q.jpg",
+      "https://farm2.staticflickr.com/1710/24503208499_bdfb1e2583_q.jpg",
+      "https://farm2.staticflickr.com/1650/24575342780_e66b987bc6_q.jpg",
+      "https://farm2.staticflickr.com/1672/24870790205_695e2dee96_q.jpg",
+      "https://farm2.staticflickr.com/1538/24844531776_117088d677_q.jpg",
+      "https://farm2.staticflickr.com/1688/24777365301_5fe568e0c1_q.jpg"
+    ]
+
+
+    @responsed_photo_result = [
+      "https://farm2.staticflickr.com/1690/24302286974_e0c00e989f_q.jpg",
+      "https://farm2.staticflickr.com/1593/24302287364_a071fd6a88_q.jpg",
+      "https://farm2.staticflickr.com/1572/24302287374_654f2758c9_q.jpg",
+      "https://farm2.staticflickr.com/1699/24302287604_b6753d19f9_q.jpg",
+      "https://farm2.staticflickr.com/1502/24302287934_b50fa68974_q.jpg",
+      "https://farm2.staticflickr.com/1718/24306046133_cd20720b0c_q.jpg",
+      "https://farm2.staticflickr.com/1605/24306046523_5d004949ce_q.jpg",
+      "https://farm2.staticflickr.com/1477/24814934032_dbacd8038c_q.jpg",
+      "https://farm2.staticflickr.com/1708/24814934672_e4e19300e7_q.jpg",
+      "https://farm2.staticflickr.com/1480/24814934682_89d17c58f1_q.jpg"
+    ]
+
+  end
+end
