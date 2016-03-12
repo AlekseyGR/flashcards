7e2c91137fe114a0269fe55d736f4a0da6886985
AlekGR <alek.grischenko@gmail.com>
2016-03-12 22:41:40 +0300

    Create action in parse cards controller.

--- app/controllers/dashboard/parse_cards_controller.rb
+++ app/controllers/dashboard/parse_cards_controller.rb
@@ -3,7 +3,14 @@ module Dashboard
     def new; end
 
     def create
+      AddCardsFromUrlJob.perform_later(parse_cards_params)
+      redirect_to cards_path, notice: "Cards adding task from #{parse_cards_params[:url]} was created"
+    end
 
+    private
+
+    def parse_cards_params
+      params.require(:parse_cards).permit(:user_id, :url, :original_text_selector, :translated_text_selector, :block_id)
     end
   end
 end
