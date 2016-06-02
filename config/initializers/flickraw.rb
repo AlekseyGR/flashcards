5b3528e66143edb940471843090318a16e37e6b0
AlekGR <alek.grischenko@gmail.com>
2016-06-02 12:46:33 +0300

    Fix tests

--- config/initializers/flickraw.rb
+++ config/initializers/flickraw.rb
@@ -1,2 +1,2 @@
-FlickRaw.api_key = Figaro.env.flickr_api_key
-FlickRaw.shared_secret = Figaro.env.flickr_api_secret
+FlickRaw.api_key = "#{Rails.application.secrets.flickr_api_key}" # Figaro.env.flickr_api_key
+FlickRaw.shared_secret = "#{Rails.application.secrets.flickr_api_secret}" # Figaro.env.flickr_api_secret
